b33061dc5fe71a0ccf893d1f7d43fd47f24dd6da
devJm95 <162802812+devJm95@users.noreply.github.com>
2024-05-18 23:10:40 +0900

    Dev (#11)
    * Spring docs 추가
    
    * Spring docs 추가

---
+++ src/main/java/com/toy/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.toy.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                    .info(new Info()
+                    .title("Toy REST API DOC")
+                    .version("1.0.0")
+                    .description("Toy REST API DOC"));
+    }
+}
--- src/main/java/com/toy/controller/board/BoardController.java
+++ src/main/java/com/toy/controller/board/BoardController.java
@@ -4,6 +4,8 @@ import com.toy.dto.board.BoardDTO;
 import com.toy.dto.board.BoardRequestDTO;
 import com.toy.entity.board.Board;
 import com.toy.service.board.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,35 +13,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Tag(name = "Board API")
 @RequestMapping("/board")
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
 
-    //insert
     @PostMapping
+    @Operation(summary = "Board 등록", description = "게시판 정보를 등록합니다")
     public ResponseEntity insertBoard(@RequestBody BoardRequestDTO boardRequestDto){
         Board result = boardService.insertBoard(boardRequestDto);
         return ResponseEntity.ok().body(result);
     }
 
-    // 게시판 조회
-    // TODO : selectAll 제거 (rest api 기본원칙은 행위는 적지않음)
     @GetMapping
+    @Operation(summary = "Board List 조회", description = "게시판 리스트를 조회합니다")
     public List<BoardDTO> getBaordList(){
         return boardService.getBaordList();
     }
 
-    // selectOne
     @GetMapping("/{id}")
+    @Operation(summary = "Board One 조회", description = "게시글 1개를 조회합니다")
     public ResponseEntity getBoardOne(@PathVariable("id") Long id){
         Board result = boardService.boardOne(id);
         return ResponseEntity.ok().body(result);
     }
 
-    // update
     @PutMapping("/{id}")
+    @Operation(summary = "Board Update", description = "게시판 정보를 수정합니다")
     public ResponseEntity updateBoard(@PathVariable("id") Long id,
                                         @RequestBody BoardRequestDTO boardRequestDto){
         Board board = boardService.updateBoard(id, boardRequestDto);
@@ -47,6 +49,7 @@ public class BoardController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Board Delete", description = "게시판 정보를 삭제합니다")
     public ResponseEntity deleteBoardOne(@PathVariable("id") Long id){
         boardService.deleteBoardOne(id);
         return ResponseEntity.ok().build();
