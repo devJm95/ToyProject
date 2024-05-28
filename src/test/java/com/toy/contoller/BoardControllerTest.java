00b97da9b7d0fdacdc0d2062e8c4ff5210f7d430
devJm95 <leehopl2@gmail.com>
2024-05-28 22:34:51 +0900

    테스트 코드 수정

--- src/test/java/com/toy/contoller/BoardControllerTest.java
+++ src/test/java/com/toy/contoller/BoardControllerTest.java
@@ -10,9 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
@@ -36,7 +40,10 @@ public class BoardControllerTest {
 
         Board board = Board.toEntity(requestDTO);
 
-        Mockito.when(boardService.insertBoard(Mockito.any(BoardRequestDTO.class))).thenReturn(board);
+//        Mockito.when(boardService.insertBoard(Mockito.any(BoardRequestDTO.class))).thenReturn(board);
+        when(boardService.insertBoard(Mockito.any(BoardRequestDTO.class))).thenReturn(board);
+        when(jwtTokenProvider.validateToken(Mockito.anyString())).thenReturn(true);  // JWT 토큰 검증 모킹
+        when(jwtTokenProvider.getAuthentication(Mockito.anyString())).thenReturn(new UsernamePasswordAuthenticationToken("user", null, new ArrayList<>())); // 인증 모킹
 
         mockMvc.perform(MockMvcRequestBuilders.post("/board")
                 .header("Authorization", "Bearer $jwtToken")
